d58ab63c2436119d12f5442e258f97597307ea85
Val <138253306+Donj63000@users.noreply.github.com>
2025-06-20 21:49:47 +0200

    Improve profanity detection

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -6,4 +6,6 @@ def test_is_exact_match_variations():
     assert ia.is_exact_match("pu te", "pute")
     assert ia.is_exact_match("pu!te", "pute")
     assert ia.is_exact_match("fil s de pu te", "fils de pute")
+    assert ia.is_exact_match("pût3", "pute")
+    assert ia.is_exact_match("filz de pute", "fils de pute")
     assert not ia.is_exact_match("computers", "pute")
