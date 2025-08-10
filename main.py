643e25945477b3f8d5e3c6f12a93c0b75f5f665c
Val <138253306+Donj63000@users.noreply.github.com>
2025-08-10 22:43:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ class EvoBot(commands.Bot):
             "calcul",
             "defender",
             "moderation",
+            "iastaff",
         ]
         for ext in extensions:
             try:
